9cd6d41c6eed03260aef13e7c2864f64d03736bb
Tabitha Lee <tabii12@gmail.com>
2020-02-03 14:56:20 -0800

    added audio for thrusting

--- 3_Project_Boost/3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/3_Project_Boost/Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
 	Rigidbody rigidBody;
+	AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -21,8 +23,16 @@ public class Rocket : MonoBehaviour {
 	{
 		if (Input.GetKey(KeyCode.Space))
 		{
-			print("Thrusting");
+			// print("Thrusting");
 			rigidBody.AddRelativeForce(Vector3.up);
+			if (!audioSource.isPlaying)
+			{
+				audioSource.Play();
+			}
+		}
+		else
+		{
+			audioSource.Stop();
 		}
 
 		if (Input.GetKey(KeyCode.A))
