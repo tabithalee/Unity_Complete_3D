7a25aac51912045af57e4eb09894c8c7eda9e4f4
Tabitha Lee <tabii12@gmail.com>
2020-02-03 19:34:58 -0800

    added serializefield for main and rotation thrust

--- 3_Project_Boost/3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/3_Project_Boost/Assets/Rocket.cs
@@ -8,23 +8,29 @@ public class Rocket : MonoBehaviour {
 	Rigidbody rigidBody;
 	AudioSource audioSource;
 
+	[SerializeField] float rcsThrust = 100f;
+	[SerializeField] float mainThrust = 450f;
+
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
 		audioSource = GetComponent<AudioSource>();
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
-		ProcessInput();
+		Thrust();
+		Rotate();
 	}
 
-	private void ProcessInput()
+
+	private void Thrust()
 	{
 		if (Input.GetKey(KeyCode.Space))
 		{
 			// print("Thrusting");
-			rigidBody.AddRelativeForce(Vector3.up);
+			rigidBody.AddRelativeForce(Vector3.up * mainThrust);
 			if (!audioSource.isPlaying)
 			{
 				audioSource.Play();
@@ -34,14 +40,24 @@ public class Rocket : MonoBehaviour {
 		{
 			audioSource.Stop();
 		}
+	}
+
+
+	private void Rotate()
+	{
+		rigidBody.freezeRotation = true; // take manual control of rotation
+
+		float rotationThisFrame = rcsThrust * Time.deltaTime;
 
 		if (Input.GetKey(KeyCode.A))
 		{
-			transform.Rotate(Vector3.forward);
+			transform.Rotate(Vector3.forward * rotationThisFrame);
 		}
 		else if (Input.GetKey(KeyCode.D))
 		{
-			transform.Rotate(-Vector3.forward);
+			transform.Rotate(-Vector3.forward * rotationThisFrame);
 		}
+
+		rigidBody.freezeRotation = false; // resume physics control of rotation
 	}
 }
