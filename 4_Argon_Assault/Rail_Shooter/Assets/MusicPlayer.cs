a4bd323d585469e35d10d7601465f1a73f81e21a
Tabitha Lee <tabii12@gmail.com>
2020-02-24 20:57:37 -0800

    x and y can now move, rotation not implemented yet

---
+++ 4_Argon_Assault/Rail_Shooter/Assets/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+	void Awake()
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadFirstScene", 10.0f);
+	}
+
+	void LoadFirstScene()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
---
+++ 4_Argon_Assault/Rail_Shooter/Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	[Tooltip("In ms^-1")][SerializeField] float xySpeed = 20f;
+	[SerializeField] float xRangeMax = 5f;
+	[SerializeField] float xRangeMin = -9.4f;
+	[SerializeField] float yRangeMax = 3f;
+	[SerializeField] float yRangeMin = -3.8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+		float xOffset = xThrow * xySpeed * Time.deltaTime;
+		float rawXPos = transform.localPosition.x + xOffset;
+
+		float yOffset = yThrow * xySpeed * Time.deltaTime;
+		float rawYPos = transform.localPosition.y + yOffset;
+
+		float clampedXPos = Mathf.Clamp(rawXPos, xRangeMin, xRangeMax);
+		float clampedYPos = Mathf.Clamp(rawYPos, yRangeMin, yRangeMax);
+
+		transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+	}
+}
