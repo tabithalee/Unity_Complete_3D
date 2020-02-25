66b951f450b366e095a55ca6f588219ef1989367
Tabitha Lee <tabii12@gmail.com>
2020-02-25 01:04:09 -0800

    added pitch, yaw and roll for smoother movement, reset van container to zero

--- 4_Argon_Assault/Rail_Shooter/Assets/Player.cs
+++ 4_Argon_Assault/Rail_Shooter/Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -6,10 +7,16 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour {
 
 	[Tooltip("In ms^-1")][SerializeField] float xySpeed = 20f;
-	[SerializeField] float xRangeMax = 5f;
-	[SerializeField] float xRangeMin = -9.4f;
-	[SerializeField] float yRangeMax = 3f;
-	[SerializeField] float yRangeMin = -3.8f;
+	[Tooltip("In m")][SerializeField] float xRange = 8f;
+	[Tooltip("In m")] [SerializeField] float yRange = 3.5f;
+
+	[SerializeField] float positionPitchFactor = -5f;
+	[SerializeField] float controlPitchFactor = -30f;
+	[SerializeField] float positionYawFactor = 3f;
+	[SerializeField] float controlRollFactor = -20f;
+
+
+	float xThrow, yThrow;
 
 	// Use this for initialization
 	void Start () {
@@ -17,9 +24,29 @@ public class Player : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-		float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+	void Update ()
+	{
+		ProcessTranslation();
+		ProcessRotation();
+	}
+
+	private void ProcessRotation()
+	{
+		float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+		float pitchDueToControlThrow = yThrow * controlPitchFactor;
+		float pitch = pitchDueToPosition + pitchDueToControlThrow;
+		
+		float yaw = transform.localPosition.x * positionYawFactor;
+		
+		float roll = xThrow * controlRollFactor;
+		
+		transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+	}
+
+	private void ProcessTranslation()
+	{
+		xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
 		float xOffset = xThrow * xySpeed * Time.deltaTime;
 		float rawXPos = transform.localPosition.x + xOffset;
@@ -27,8 +54,8 @@ public class Player : MonoBehaviour {
 		float yOffset = yThrow * xySpeed * Time.deltaTime;
 		float rawYPos = transform.localPosition.y + yOffset;
 
-		float clampedXPos = Mathf.Clamp(rawXPos, xRangeMin, xRangeMax);
-		float clampedYPos = Mathf.Clamp(rawYPos, yRangeMin, yRangeMax);
+		float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+		float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
 		transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
 	}
