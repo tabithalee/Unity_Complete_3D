a3567f5ffa6b9c15cd872c21f31a412bcd6b88eb
Tabitha Lee <tabii12@gmail.com>
2020-02-29 13:03:17 -0800

    some crazy synth layering audio stuff

--- 4_Argon_Assault/Rail_Shooter/Assets/Scripts/MusicPlayer.cs
+++ 4_Argon_Assault/Rail_Shooter/Assets/Scripts/MusicPlayer.cs
@@ -4,6 +4,11 @@ public class MusicPlayer : MonoBehaviour {
 
 	void Awake()
 	{
+		int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+		print("music players in this scene: " + numMusicPlayers);
+		// if more than one music player in scene
+			// destroy ourselves
+		// else
 		DontDestroyOnLoad(this.gameObject);
 	}
 }
