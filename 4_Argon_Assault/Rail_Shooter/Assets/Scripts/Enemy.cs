8d83bf1aedee55ee53000364938acbe8c551001e
Tabitha Lee <tabii12@gmail.com>
2020-03-02 12:54:05 -0800

    score now updates when enemies are destroyed

--- 4_Argon_Assault/Rail_Shooter/Assets/Scripts/Enemy.cs
+++ 4_Argon_Assault/Rail_Shooter/Assets/Scripts/Enemy.cs
@@ -6,13 +6,24 @@ public class Enemy : MonoBehaviour {
 
 	[SerializeField] GameObject deathFX;
 	[SerializeField] Transform parent;
+	[SerializeField] int scorePerHit = 12;
+
+
+	ScoreBoard scoreBoard;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		AddBoxCollider();
+		scoreBoard = FindObjectOfType<ScoreBoard>();
+	}
+
+	private void AddBoxCollider()
+	{
 		Collider boxCollider = gameObject.AddComponent<BoxCollider>();
 		boxCollider.isTrigger = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
@@ -22,6 +33,7 @@ public class Enemy : MonoBehaviour {
     {
 		GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
 		fx.transform.parent = parent;
+		scoreBoard.ScoreHit(scorePerHit);
 		Destroy(gameObject);
     }
 }
