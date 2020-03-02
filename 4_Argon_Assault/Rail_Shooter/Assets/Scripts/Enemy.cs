f01eafd5b6627f6933ccf9ba2144da5b8cc3d355
Tabitha Lee <tabii12@gmail.com>
2020-03-01 23:04:41 -0800

    enemies now automatically adding their own box colliders

---
+++ 4_Argon_Assault/Rail_Shooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+		boxCollider.isTrigger = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnParticleCollision(GameObject other)
+    {
+		Destroy(gameObject);
+    }
+}
--- 4_Argon_Assault/Rail_Shooter/Assets/Scripts/MusicPlayer.cs
+++ 4_Argon_Assault/Rail_Shooter/Assets/Scripts/MusicPlayer.cs
@@ -5,10 +5,14 @@ public class MusicPlayer : MonoBehaviour {
 	void Awake()
 	{
 		int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
-		print("music players in this scene: " + numMusicPlayers);
-		// if more than one music player in scene
-			// destroy ourselves
-		// else
-		DontDestroyOnLoad(this.gameObject);
+
+		if (numMusicPlayers > 1) 
+		{
+			Destroy(this.gameObject);
+		}
+		else
+		{
+			DontDestroyOnLoad(this.gameObject);
+		}
 	}
 }
