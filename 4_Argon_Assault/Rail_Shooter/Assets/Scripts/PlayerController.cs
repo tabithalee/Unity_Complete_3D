eefda10150e1e0fec6ac3c63b8e84735a3fe0848
tabithalee <leetabithagemma@gmail.com>
2020-03-25 17:34:22 -0700

    adjusted gun of prefab angle, changed emissions to continue after space bar released

--- 4_Argon_Assault/Rail_Shooter/Assets/Scripts/PlayerController.cs
+++ 4_Argon_Assault/Rail_Shooter/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ using UnityStandardAssets.CrossPlatformInput;
 public class PlayerController : MonoBehaviour {
 
 	[Header("General")]
-	[Tooltip("In ms^-1")][SerializeField] float controlSpeed = 20f;
+	[Tooltip("In ms^-1")][SerializeField] float controlSpeed = 30f;
 	[Tooltip("In m")][SerializeField] float xRange = 9f;
 	[Tooltip("In m")] [SerializeField] float yRange = 5f;
 
@@ -71,29 +71,21 @@ public class PlayerController : MonoBehaviour {
 	{
 		if (CrossPlatformInputManager.GetButton("Fire"))
 		{
-			ActivateGuns();
+			SetGunsActive(true);
 		}
 		else
 		{
-			DeactivateGuns();
+			SetGunsActive(false);
 		}
 	}
 
 
-	private void ActivateGuns()
+	private void SetGunsActive(bool isActive)
 	{
-		foreach (GameObject gun in guns)
+		foreach (GameObject gun in guns) // care may affect death FX
 		{
-			gun.SetActive(true);
-		}
-	}
-
-
-	private void DeactivateGuns()
-	{
-		foreach (GameObject gun in guns)
-		{
-			gun.SetActive(false);
+			var emissionModule = gun.GetComponent<ParticleSystem>().emission;
+			emissionModule.enabled = isActive;
 		}
 	}
 
