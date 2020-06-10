054ac7ffc09b3a61e55bd9f29d93910e0e9a541b
tabithalee <leetabithagemma@gmail.com>
2020-06-10 15:26:45 -0700

    added some quad cubes and a grid

---
+++ 5_Realm_Rush_Tower/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+[SelectionBase] // this is the object we want to select
+public class CubeEditor : MonoBehaviour {
+ 
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        textMesh.text = gameObject.name = labelText;
+    }
+}
---
+++ 5_Realm_Rush_Tower/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	[SerializeField] List<Waypoint> path;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(FollowPath());
+	}
+	
+	IEnumerator FollowPath ()
+	{
+		print("Starting patrol...");
+
+		foreach (Waypoint waypoint in path)
+		{
+			transform.position = waypoint.transform.position;
+			print("Visiting block: " + waypoint);
+			yield return new WaitForSeconds(1f);
+		}
+
+		print("Ending patrol");
+	}
+}
