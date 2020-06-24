5c4a0274e2fd89ce74915472f51fa98f5dbfdcdf
tabithalee <leetabithagemma@gmail.com>
2020-06-23 17:18:13 -0700

    Towers targeting closest enemy

--- 5_Realm_Rush_Tower/Assets/Scripts/Tower.cs
+++ 5_Realm_Rush_Tower/Assets/Scripts/Tower.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour {
 
+	// Parameters
 	[SerializeField] Transform objectToPan;
-	[SerializeField] Transform targetEnemy;
-
 	[SerializeField] float attackRange = 20f;
 	[SerializeField] ParticleSystem projectileParticle;
-	
+
+	// State
+	Transform targetEnemy;
+
 	// Update is called once per frame
 	void Update () {
+
+		SetTargetEnemy();
 		if (targetEnemy)
 		{
 			objectToPan.LookAt(targetEnemy);
@@ -22,6 +26,34 @@ public class Tower : MonoBehaviour {
 		{
 			Shoot(false);
 		}
+	}
+
+	private void SetTargetEnemy()
+	{
+		var sceneEnemies = FindObjectsOfType<EnemyDamage>();
+		if (sceneEnemies.Length == 0) { return; }
+
+		Transform closestEnemy = sceneEnemies[0].transform;
+
+		foreach (EnemyDamage testEnemy in sceneEnemies)
+		{
+			closestEnemy = GetClosest(closestEnemy, testEnemy.transform);
+		}
+
+		targetEnemy = closestEnemy;
+	}
+
+	private Transform GetClosest(Transform transformA, Transform transformB)
+	{
+		var distToA = Vector3.Distance(transform.position, transformA.position);
+		var distToB = Vector3.Distance(transform.position, transformB.position);
+
+		if (distToA < distToB)
+		{
+			return transformA;
+		}
+
+		return transformB;
 	}
 
 	private void FireAtEnemy()
