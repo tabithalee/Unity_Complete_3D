4832c38be13cee7d3d37dad535e63a1dd3b465e6
tabithalee <leetabithagemma@gmail.com>
2020-07-31 16:57:07 -0700

    added raycasting to player for shooting

---
+++ 6_Zombie_Runner/Assets/Scripts/Weapons.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+
+    void Update()
+    {
+        // using mouse button instead of a key
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+        Debug.Log("Hit " + hit.transform.name);
+    }
+}
